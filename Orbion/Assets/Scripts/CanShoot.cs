2c7e1f90b27a9a4f225f781bf6e915a2af98cea8
DannyPaguiao <dpaghari@ucsc.edu>
2014-01-22 18:12:03 -0800

    Separated Player script into CanMove, CanShoot & CanShootReload as per the UML. Implemented reloading

---
+++ Orbion/Assets/Scripts/CanShoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanShoot : MonoBehaviour {
+
+	public Rigidbody bullet;										// takes in bullet_prefab
+	private Rigidbody clone;
+	public float bullet_speed = 5.0F;								// Default Bullet Speed
+	public float firingTimer = 0.0F;								// Counter for the next shot
+	public float firingRate = 20.0F;								// Cooldown for each shot
+
+	
+	// Update is called once per frame
+	void Update () {
+		firingTimer++;
+									
+		//Shoot();
+
+
+	}
+
+	public void Shoot(){													// creates an instance of bullet_prefab and shoots it towards mouse cursor
+
+													
+		if(firingTimer > firingRate){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			// Casts the ray and get the first game object hit
+			Physics.Raycast(ray, out hit);
+			Vector3 adjustY = Vector3.zero;
+			adjustY.y += 1;
+			Vector3 bullet_dir = hit.point + adjustY - transform.position;
+			bullet_dir = bullet_dir.normalized;
+			clone = Instantiate(bullet, transform.position + bullet_dir * 2, Quaternion.LookRotation(hit.point + adjustY - transform.position, Vector3.up)) as Rigidbody;
+			clone.rigidbody.velocity = bullet_dir * bullet_speed * 20;
+			firingTimer = 0.0f;
+			GetComponent<CanShootReload>().currentAmmo--;
+				
+		}
+	}
+}
---
+++ Orbion/Assets/Scripts/CanShootReload.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CanShootReload : MonoBehaviour {
+
+	public int clipSize = 5;
+	public int currentAmmo = 5;
+	public float reloadTime = 0.0F;
+	public float reloadCooldown = 200.0F;
+	public bool reloading = false;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		reloadTime++; 
+
+
+		if(Input.GetMouseButton(0)){
+			Debug.Log(currentAmmo);
+			if(currentAmmo > 0){
+			GetComponent<CanShoot>().Shoot();
+			}
+		
+		}
+
+		if(currentAmmo == 0 || (Input.GetKeyDown("r") && currentAmmo != clipSize)) 
+		   reloading = true;
+
+		if(reloading)
+			reload ();
+	
+	}
+
+	void reload () {
+
+		if(reloadTime > reloadCooldown){
+			currentAmmo = clipSize;
+		}
+
+		reloading = false;
+	}
+}
