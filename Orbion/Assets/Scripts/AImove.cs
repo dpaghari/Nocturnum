a019c1b71707ec56ce13d2293137eb1cc5bbaa9e
Mjshaar <mj.shaar@ucsc.edu>
2014-01-22 19:12:28 -0800

    add components for enemy_prefab

---
+++ Orbion/Assets/Scripts/AImove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AImove : MonoBehaviour {
+	public float speed = 6.0F;
+	//public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+	
+	void Update() {
+
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+		
+		moveDirection.y -= gravity * Time.deltaTime;
+	}
+}
