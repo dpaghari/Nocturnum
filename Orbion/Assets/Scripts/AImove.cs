982760a85370808c284e63d30d031267d1cda1c8
Mjshaar <mj.shaar@ucsc.edu>
2014-01-24 14:05:43 -0800

    AImove and killable

--- Orbion/Assets/Scripts/AImove.cs
+++ Orbion/Assets/Scripts/AImove.cs
@@ -4,15 +4,62 @@ using System.Collections;
 public class AImove : MonoBehaviour {
 	public float speed = 6.0F;
 	//public float jumpSpeed = 8.0F;
-	public float gravity = 20.0F;
+	//public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
+	public GameObject target;
+	//public float smoothTime = 0.3F;
+	//public float xOffset = 1.0F;
+	//public float yOffset = 1.0F;
+	//private var thisTransform : Transform;
+	//private var velocity : Vector2;
 	
 	void Update() {
-
-		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		moveDirection = transform.TransformDirection(moveDirection);
-		moveDirection *= speed;
+		this.target = GameObject.Find ("player_prefab");
+		Vector3 targ = target.transform.position;
 		
-		moveDirection.y -= gravity * Time.deltaTime;
+		Vector3 direction = targ - transform.position;
+		direction.Normalize();
+		transform.position += direction * speed * Time.deltaTime;
+		//CharacterController controller = GetComponent<CharacterController>();
+		/*
+			Ray ray = this.transform.position;
+			RaycastHit hit = target.position;
+			// Casts the ray and get the first game object hit
+			Physics.Raycast(ray, hit);
+			Vector3 adjustY = Vector3.zero;
+			adjustY.y += 1;
+			Vector3 bullet_dir = hit.point + adjustY - transform.position;
+			bullet_dir = bullet_dir.normalized;
+
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+		*/	
+		
+		//moveDirection.y -= gravity * Time.deltaTime;
+		//controller.Move(moveDirection * Time.deltaTime);
+		
+		//this.transform.position = Vector3.MoveTowards (this.transform.position, target.transform.position, .03F);
+		//this.transform.LookAt (target.transform);
+		//this.transform.position = target.transform.position;
+		//this.transform.rotation = target.transform.rotation;
+		//transform.position = Vector3.Lerp(this.transform.position, target.transform.position, Time.deltaTime * 10);
+	}
+	
+	void FixedUpdate() {
+		//this.transform.position = Vector3.MoveTowards (this.transform.position, target.transform.position, .03F);
+		//this.transform.LookAt (target.transform);
+		/*
+		Ray ray = Camera.main.ScreenPointToRay(target.rigidbody.transform.position);
+		RaycastHit hit;
+		// Casts the ray and get the first game object hit
+		Physics.Raycast(ray, out hit);
+		Vector3 adjustY = Vector3.zero;
+		adjustY.y += 1;
+		Vector3 enemy_dir = hit.point + adjustY - transform.position;
+		rigidbody.velocity = enemy_dir;
+		*/
+		
+		
 	}
 }
---
+++ Orbion/Assets/Scripts/Killable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killable : MonoBehaviour {
+	//private double maxHP;
+	private double currHP; 
+	private double dmg = 5;
+	public double baseHP = 10;
+	public GameObject deathTarget;
+	// Use this for initialization
+	void Start () {
+		currHP = baseHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void damage (double dmg) {
+		currHP -= dmg;
+		if (currHP <= 0.0) {
+			kill();
+		}
+	}
+	
+	void kill () {
+		//destory object
+		//make death object
+	}
+	
+}
+
