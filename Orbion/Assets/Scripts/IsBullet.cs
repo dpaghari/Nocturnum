6ffba0de09ee5f55f9ffd3a341c8ad592811933b
rswartzm <rswartzm@ucsc.edu>
2014-01-24 14:21:39 -0800

    Actually committed the IsBullet script this time.

---
+++ Orbion/Assets/Scripts/IsBullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsBullet : MonoBehaviour {
+	public int lifetime = 100;
+	public int damage = 5;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifetime--;
+		if(lifetime < 0)
+			Destroy (this.gameObject);
+	}
+}
