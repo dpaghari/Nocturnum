563241f2a1f41a939bff6d020514bcd71967fe90
William Situ <wsitu@ucsc.edu>
2014-01-24 19:03:39 -0800

    Added AvatarController, Game and Gui Manager
    GameManager holds a reference to the player
    No implementation  was added to AvatarController and GuiManager

---
+++ Orbion/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//Global stuff for miscellanous objects
+//If a lot of functionality is being added to a certain object in here,
+//it should be moved to it's own separate singleton class
+public class GameManager : Singleton<GameManager> {
+
+	protected GameManager() {} // guarantee this will be always a singleton only - can't use the constructor!
+
+	private GameObject _Player;
+
+	public static bool HasPlayer() { return Instance._Player != null; }
+
+	public static GameObject Player{
+		get {
+			if (!HasPlayer() ) {
+				Instance._Player = (GameObject.FindObjectOfType(typeof(AvatarController)) as AvatarController).gameObject;
+				if(! HasPlayer() )
+					Debug.Log("Error: Could not find a player object.");
+			}
+			return Instance._Player;
+		}
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Orbion/Assets/Scripts/GuiManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiManager : Singleton<GuiManager> {
+
+	protected GuiManager() {} // guarantee this will be always a singleton only - can't use the constructor!
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
