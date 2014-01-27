1987314b29e4d37368cbb32820843502b41d252d
jnobleza <jnobleza@ucsc.edu>
2014-01-27 01:49:32 -0800

    Added temporary placeholder for light and energy on the UI (no graphics yet, but numbers do show), and code that can be commented in for when the manager is attached to a game object.

--- Orbion/Assets/Scripts/UserInterface.cs
+++ Orbion/Assets/Scripts/UserInterface.cs
@@ -9,18 +9,29 @@ public class UserInterface : MonoBehaviour {
 	public Texture2D health_bar_40;
 	public Texture2D health_bar_20;
 	public Texture2D health_bar_0;
-	public int player_Health;
 	private double gameTimeSec = 0.0;
 	private double gameTimeMin = 0.0;
 	private double gameTimeHours = 0.0;
 	public GUISkin uiSkin;
 	public GameObject ammoRef;
+	public GameObject resRef;
+
+	//Temporary variable placeholders for health, energy, and Light
+	public int player_Health;
+	public int player_Energy;
+	public int player_Light;
 
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating ("addTime", 1, 1);
 		ammoRef = GameObject.Find ("player_prefab");
+		//Code for when the stuff is attached to a GameObject.
+		//resRef = GameObject.Find("     ");
+
+		//Temporary placeholders for testing numbers.
 		player_Health = 100;
+		player_Energy = 50;
+		player_Light = 30;
 	}
 
 	void addTime(){
@@ -38,17 +49,19 @@ public class UserInterface : MonoBehaviour {
 		GUI.skin = uiSkin;
 
 		// Health Bar
-		GUI.DrawTexture(new Rect(Screen.width/2-80, Screen.height-85, health_bar_100.width/6, health_bar_100.height/3), health_bar_100);
 
-		if(player_Health < 80){
+
+		if(player_Health > 80){
+			GUI.DrawTexture(new Rect(Screen.width/2-80, Screen.height-85, health_bar_100.width/6, health_bar_100.height/3), health_bar_100);
+		} else if(player_Health > 60){
 			GUI.DrawTexture(new Rect(Screen.width/2-80, Screen.height-85, health_bar_80.width/6, health_bar_80.height/3), health_bar_80);
-		} else if(player_Health < 60){
+		} else if(player_Health > 40){
 			GUI.DrawTexture(new Rect(Screen.width/2-80, Screen.height-85, health_bar_60.width/6, health_bar_60.height/3), health_bar_60);
-		} else if(player_Health < 40){
+		} else if(player_Health > 20){
 			GUI.DrawTexture(new Rect(Screen.width/2-80, Screen.height-85, health_bar_40.width/6, health_bar_40.height/3), health_bar_40);
-		} else if(player_Health < 20){
+		} else if(player_Health > 0){
 			GUI.DrawTexture(new Rect(Screen.width/2-80, Screen.height-85, health_bar_20.width/6, health_bar_20.height/3), health_bar_20);
-		} else if(player_Health < 20){
+		} else if(player_Health == 0){
 			GUI.DrawTexture(new Rect(Screen.width/2-80, Screen.height-85, health_bar_0.width/6, health_bar_0.height/3), health_bar_0);
 		}
 
@@ -69,5 +82,13 @@ public class UserInterface : MonoBehaviour {
 		} else {
 			GUI.Label(new Rect(5, 40, 150, 50), ammoRef.GetComponent<CanShootReload>().currentAmmo + "/" + ammoRef.GetComponent<CanShootReload>().clipSize);
 		}
+
+		//Light
+		GUI.Label(new Rect(5, Screen.height-25, 150, 50), "Light: " + player_Light);
+		//GUI.Label(new Rect(5, Screen.height-25, 150, 50), "Light: " + resRef.GetComponent<ResManager>().LightRes);
+
+		//Energy
+		GUI.Label(new Rect(110, Screen.height-25, 150, 50), "Energy: " + player_Energy);
+		//GUI.Label(new Rect(5, Screen.height-25, 150, 50), "Energy: " + resRef.GetComponent<ResManager>().UsedEnergy + "/" + resRef.GetComponent<ResManager>().MaxEnergy);
 	}
 }
