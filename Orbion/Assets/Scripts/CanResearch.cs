d08a964847bdbe454b98cdfe37bfe326cebb00bd
William Situ <wsitu@ucsc.edu>
2014-01-28 05:15:17 -0800

    skeleton for upgrading
    added CanResearch class for making upgrade menu
    player can open up research menu by pressing v

---
+++ Orbion/Assets/Scripts/CanResearch.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanResearch : MonoBehaviour {
+
+	private bool menuUp = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	void OnGUI() {
+		if(menuUp){
+			GetComponent<CanShoot>().ResetFiringTimer();
+			GUI.Box(new Rect (10,10,100,90), "Research Menu");
+			
+			if(GUI.Button(new Rect(20,40,80,20), "Scattershot")) {
+				TechManager.Research(Tech.scatter);
+				menuUp = false;
+			}
+
+		}
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.V)){
+			menuUp = !menuUp;
+		}
+	
+	}
+}
