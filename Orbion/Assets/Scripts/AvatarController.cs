fdd08a1d82c8a79d2026420d789821ba61f505c7
William Situ <wsitu@ucsc.edu>
2014-01-31 23:20:32 -0800

    fixed two issues with scattershot and ammo
    player now auto-reloads if shooting with 0 ammo
    player no longer shoots lopsided scattershots if the amount of ammo we have is less than what scatter can shoot

--- Orbion/Assets/Scripts/AvatarController.cs
+++ Orbion/Assets/Scripts/AvatarController.cs
@@ -32,12 +32,19 @@ public class AvatarController : MonoBehaviour {
 	//Works even if we're just shooting 1 bullet.
 	protected void Scattershot(Vector3 target){
 		Vector3 dir = target - transform.position;
+
+		//if we have no ammo the for loop below won't run and the player won't auto reload
+		//so forcing it to happen here
+		if( shootScript.currentAmmo <= 0)
+			shootScript.Shoot(dir);		
+
 		if( shootScript.FinishCooldown()){
 			//Vector3 hitAngle = adjustedHit - transform.position;
 			Vector3 leftBound = Quaternion.Euler( 0, -ScatterSpread/2, 0) * dir;
 			int ScatterCount = TechManager.GetUpgradeLv( Tech.scatter) + 1;
-			for ( int i = 1; i <= ScatterCount; i++){
-				float angleOffset = i * ( ScatterSpread / ( ScatterCount + 1));
+			int NumBulletsToShoot = Mathf.Min(ScatterCount, shootScript.currentAmmo);
+			for ( int i = 1; i <= NumBulletsToShoot; i++){
+				float angleOffset = i * ( ScatterSpread / ( NumBulletsToShoot + 1));
 				Vector3 BulDir = Quaternion.Euler( 0, angleOffset, 0) * leftBound ;
 				shootScript.SetFiringTimer( 1.0f);
 				shootScript.ShootDir( BulDir);
