1c1333264ce6113b37f22bf02e633d5ee868b88d
DannyPaguiao <dpaghari@ucsc.edu>
2014-02-04 22:42:48 -0800

    Cleaned up code

--- Orbion/Orbion/Assets/Scripts/IsWeakened.cs
+++ Orbion/Orbion/Assets/Scripts/IsWeakened.cs
@@ -7,20 +7,20 @@ public class IsWeakened : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
-		Debug.Log ("GAY");
+
 		if(other.tag == "lightsource")
 		GetComponent<AIController>().isWeakened = true;
 
 	}
 	void OnTriggerStay(Collider other)
 	{
-		Debug.Log ("WEINER");
+
 		if(other.tag == "lightsource")
 		GetComponent<AIController>().isWeakened = true;
 	}
 	void OnTriggerExit(Collider other)
 	{
-		Debug.Log ("LOL");
+
 		if(other.tag == "lightsource")
 		GetComponent<AIController>().isWeakened = false;
 	}
