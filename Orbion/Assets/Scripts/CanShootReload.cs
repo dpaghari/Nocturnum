641a67f8af7f2e23af3a537876f4d7d79a83a42c
Mjshaar <mj.shaar@ucsc.edu>
2014-02-04 20:22:04 -0800

    <MetricManager> - Tracks How many times player fires, to use - MetricManager.ShotsFired

--- Orbion/Assets/Scripts/CanShootReload.cs
+++ Orbion/Assets/Scripts/CanShootReload.cs
@@ -72,6 +72,7 @@ public class CanShootReload : CanShoot {
 			if(currentAmmo > 0 ){
 				base.ShootDir(dir);
 				currentAmmo--;
+				MetricManager.AddShots (1);
 			}
 
 			else {
--- Orbion/Assets/Scripts/MetricManager.cs
+++ Orbion/Assets/Scripts/MetricManager.cs
@@ -5,23 +5,20 @@ public class MetricManager : Singleton<MetricManager> {
 	
 	protected MetricManager() {} // guarantee this will be always a singleton only - can't use the constructor!
 	
-	private static int shotsFired = 0;
+	private int _shotsFired = 0;
+
+	public static void AddShots(int amt){ Instance._shotsFired += amt;}
+	public static int ShotsFired{ get { return Instance._shotsFired;}}
+
+	/*
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
-	public static void incrementShots(){
-		shotsFired++;
-	}
-
-	public static int getShots(){
-		return shotsFired;
-	}
-
+	*/
 }
