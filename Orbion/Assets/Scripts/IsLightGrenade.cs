7e7bbb840ed87cdd2bc0495f55273eb417c28955
DannyPaguiao <dpaghari@ucsc.edu>
2014-02-06 14:23:12 -0800

    Added light_grenade prefab -attached light_source prefab to light_grenade prefab -attached isLightGrenade script to the light_grenade which creates a light_source prefab when colliding with ground -generator light removed, anything with a light_source should attach a light_source to its prefab, this will emit the light

---
+++ Orbion/Assets/Scripts/IsLightGrenade.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsLightGrenade : MonoBehaviour {
+
+	//public AIController enemyStat;
+	public GameObject flare;
+	void OnCollisionEnter(Collision other)
+	{
+		
+		if(other.gameObject.tag == "ground"){
+			Debug.Log ("hit ground");
+			Instantiate(flare, transform.position, Quaternion.identity);
+
+		}
+		Destroy(gameObject);
+		
+	}
+
+
+}
