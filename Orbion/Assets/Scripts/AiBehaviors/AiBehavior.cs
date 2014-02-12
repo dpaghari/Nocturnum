aaf788ab3543a4d998a42d5a5c32acf45a8eb4bc
William Situ <wsitu@ucsc.edu>
2014-02-12 01:53:38 -0800

    base for AI behavior

---
+++ Orbion/Assets/Scripts/AiBehaviors/AiBehavior.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AiBehavior : MonoBehaviour {
+
+	abstract public void OnBehaviorEnter();
+
+	abstract public void OnBehaviorExit();
+
+	abstract public void FixedUpdateAB();
+
+	abstract public void UpdateAB();
+}
