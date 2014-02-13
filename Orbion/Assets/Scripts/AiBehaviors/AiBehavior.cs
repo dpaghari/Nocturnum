cb167a9aee5f1a62b54f38f0d38930685e354b27
William Situ <wsitu@ucsc.edu>
2014-02-12 23:55:46 -0800

    Reorganized Ai behavior system

--- Orbion/Assets/Scripts/AiBehaviors/AiBehavior.cs
+++ Orbion/Assets/Scripts/AiBehaviors/AiBehavior.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+//The AI behaviors run actions from components
 public abstract class AiBehavior : MonoBehaviour {
 
+	//initialization of the behavior
 	abstract public void OnBehaviorEnter();
-
+	
+	//cleanup/transitions when leaving this behavior
 	abstract public void OnBehaviorExit();
-
+	
+	//Stuff we run on FixedUpdate when this is the current behavior
 	abstract public void FixedUpdateAB();
-
+	
+	//Stuff we run on Update when this behavior is the current running
 	abstract public void UpdateAB();
 }
---
+++ Orbion/Assets/Scripts/AiBehaviors/AiController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//This is a very basic AI controller that just runs one behavior.
+
+//Derive more advanced controllers from this class.
+
+//AiControllers should contain logic to manage behaviors
+public class AiController : MonoBehaviour {
+
+	public AiBehavior CurrBehavior;	
+	
+	protected virtual void Start () {
+		if (CurrBehavior == null)
+			CurrBehavior = GetComponent<AiBehavior>();
+		CurrBehavior.OnBehaviorEnter();
+	}
+	
+	
+	protected virtual void FixedUpdate () {
+		CurrBehavior.FixedUpdateAB();
+	}
+	
+	
+	
+	protected virtual void Update () {
+		CurrBehavior.UpdateAB();
+	}
+	
+	public virtual void SwitchBehavior( AiBehavior newBehavior){
+		CurrBehavior.OnBehaviorExit();
+		newBehavior.OnBehaviorEnter();
+		CurrBehavior = newBehavior;
+	}
+
+}
