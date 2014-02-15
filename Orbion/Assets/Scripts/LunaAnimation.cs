c924276a1db96484f919d39f1f40638dbbd02e4e
DannyPaguiao <dpaghari@ucsc.edu>
2014-02-14 20:06:37 -0800

    Fixed animation rotations Set Collision Layers for Enemy and Player since they had none resulting in the player killing itself with his or her own bullet by moving towards the direction they were shooting

--- Orbion/Assets/Scripts/LunaAnimation.cs
+++ Orbion/Assets/Scripts/LunaAnimation.cs
@@ -10,8 +10,29 @@ public class LunaAnimation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//animation.CrossFade("Idle");
+	 
+		if (Input.GetKeyDown(KeyCode.W))
+			transform.forward = new Vector3(0f, 0f, 1f);
+		else if (Input.GetKeyDown(KeyCode.A))
+			transform.forward = new Vector3(-1f, 0f, 0f);
+		else if (Input.GetKeyDown(KeyCode.S))
+			transform.forward = new Vector3(0f, 0f, -1f);
+		else if (Input.GetKeyDown(KeyCode.D))
+			transform.forward = new Vector3(1f, 0f, 0f);
+		else if (Input.GetKeyDown(KeyCode.W) && (Input.GetKeyDown(KeyCode.A)))
+			transform.forward = new Vector3(-1f, 0f, 1f);
+		else if (Input.GetKeyDown(KeyCode.W) && (Input.GetKeyDown(KeyCode.D)))
+			transform.forward = new Vector3(1f, 0f, 1f);
+		else if (Input.GetKeyDown(KeyCode.S) && (Input.GetKeyDown(KeyCode.A)))
+			transform.forward = new Vector3(-1f, 0f, -1f);
+		else if (Input.GetKeyDown(KeyCode.D))
+			transform.forward = new Vector3(1f, 0f, -1f);
 
+
+
+	if(Input.GetKeyUp("w")||Input.GetKeyUp("a")||Input.GetKeyUp("d") || Input.GetKeyUp("s")) {
+			animation.CrossFade("Idle");
+	 }
 
 	
 	}
