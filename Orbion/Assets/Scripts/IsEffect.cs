07e614611c8dda78b78def75bb429e2404925dd4
Mjshaar <mshaar@ucsc.edu>
2014-02-17 19:16:50 -0800

    (enemy_prefabs)fixed null error added on hit effects <IsEffect> just has a lifetime in it attached to (on_hit_effect) prefab

---
+++ Orbion/Assets/Scripts/IsEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsEffect : MonoBehaviour {
+
+	private float lifeTime = 5.0F;
+	private float lifeCounter = 0.0F;
+	
+	public void Update(){
+		if(lifeCounter > lifeTime){
+			Destroy(this.gameObject);
+		} else {
+			lifeCounter += Time.deltaTime;
+		}
+	}
+}
