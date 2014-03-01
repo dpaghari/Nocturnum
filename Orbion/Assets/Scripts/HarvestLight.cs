112547000630b07d7c5ce32e7257725834dc5842
Daniel Pagharion <dpaghari@ucsc.edu>
2014-02-28 17:13:47 -0800

    Player can harvest Lumen from a Generator(tagged "lightwell")

---
+++ Orbion/Assets/Scripts/HarvestLight.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarvestLight : MonoBehaviour {
+
+	public bool hasLumen = false;
+	private bool lit = false;
+	// Use this for initialization
+	void Start () {
+		Debug.Log(hasLumen);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(lit) {
+			if(Input.GetMouseButtonDown(1) && hasLumen == false){
+			hasLumen = true;
+			Debug.Log (hasLumen);
+			}
+		}
+
+
+	}
+
+	void OnTriggerStay(Collider other){
+		if(other.tag == "lightwell") 
+			lit = true;
+	}
+}
