bf1f1ffe73690531ca4ba8c6a6610bd64860da4c
capstoneCrusher <mtmcgowa@ucsc.edu>
2014-02-28 16:19:28 -0800

    player's light pushes back enemies

---
+++ Orbion/Assets/Scripts/IsLightCone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsLightCone : MonoBehaviour {
+
+	public float pushForce;
+	public ForceMode pushForceMode = ForceMode.Impulse;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay ( Collider other){
+		CanMove moveScript = other.GetComponent<CanMove>();
+		
+		if (moveScript != null){
+			Vector3 pushDir = (other.rigidbody.position - rigidbody.position).normalized;
+			other.rigidbody.AddForce(pushDir * pushForce, pushForceMode);
+		}
+	}
+}
