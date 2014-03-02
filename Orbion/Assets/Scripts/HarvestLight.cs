585eb6b84601342c64b545813117ff908315b717
DannyPaguiao <dpaghari@ucsc.edu>
2014-03-01 23:27:57 -0800

    Added Harvesting of Lumen from Light Geysers(Former Medbay Building) Shows on HUD when you harvest Lumen Right click when near a Light Geyser to Harvest Right click when near a Generator to Deposit

--- Orbion/Assets/Scripts/HarvestLight.cs
+++ Orbion/Assets/Scripts/HarvestLight.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 
 public class HarvestLight : MonoBehaviour {
-
+	public int LumenCount = 0;
 	public bool hasLumen = false;
 	private bool lit = false;
+	private bool canDeposit = false;
 	// Use this for initialization
 	void Start () {
 		Debug.Log(hasLumen);
@@ -12,7 +13,11 @@ public class HarvestLight : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+
+		// If the player is near a Light Geyser and Presses Right Click
+		// Harvest the Lumen
 		if(lit) {
 			if(Input.GetMouseButtonDown(1) && hasLumen == false){
 			hasLumen = true;
@@ -21,10 +26,36 @@ public class HarvestLight : MonoBehaviour {
 		}
 
 
+		//  If the player is near a Generator and Has Harvested Lumen
+		//  and if he presses right click it gives the generator +20 Lumen
+		if(canDeposit){
+
+			if(Input.GetMouseButtonDown(1) && hasLumen == true){
+				hasLumen = false;
+				ResManager.AddLumen(20);
+				Debug.Log(LumenCount);
+
+			}
+
+		}
+
+
 	}
 
 	void OnTriggerStay(Collider other){
 		if(other.tag == "lightwell") 
 			lit = true;
+
+
+		if(other.tag == "Generator"){
+			canDeposit = true;
+			//Debug.Log (canDeposit);
+		}
+		else{
+			canDeposit = false;
+			//Debug.Log (canDeposit);
+		}
 	}
+
+
 }
