5ab55d715f28bb0266e1d440c365e297ad3cf4b8
Mjshaar <mj.shaar@ucsc.edu>
2014-03-07 14:39:05 -0800

    <Corruption>-most of the functionality is done, haven't added it to the generator yet

---
+++ Orbion/Assets/Scripts/Corruption.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Corruptable {
+	none = 0,
+	generator,
+	enemyMelee,
+	enemyRanged
+}
+
+
+//replace killable with this in generator prefab
+public class Corruption : MonoBehaviour {
+
+	public float corruption = 0.0f;
+	private float corruptLimit = 80.0f;
+	private bool active = true;
+	public Corruptable corruptType = Corruptable.none;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void damage(float dmg){
+		if(active){
+			corruption += dmg;
+			if(corruption >= corruptLimit){
+				deactivate();
+			}
+
+		}
+	}
+
+	public void deactivate(){
+		active = false;
+		if(corruptType == Corruptable.generator) Debug.Log ("DEAD");
+	}
+
+
+}
