a4d6e4a6e09fc827932c77463240eb82c1dafc88
William Situ <wsitu@ucsc.edu>
2014-03-13 14:55:27 -0700

    fog now will target a generator if it's close enough

--- Orbion/Assets/Scripts/IsFog.cs
+++ Orbion/Assets/Scripts/IsFog.cs
@@ -3,18 +3,43 @@ using System.Collections;
 
 [RequireComponent(typeof(CanMove))]
 public class IsFog : MonoBehaviour {
-	public Rigidbody target;
+
+	//Current Target the fog is heading towards
+	public GameObject currTarget;
+
+	//The range at which the fog searches for entities other than the MainGenerator
+	public float searchRange = 10;
 
 	private Vector3 dir;
 	private CanMove moveScript;
 	
 
 
+	public static bool IsValidTarget( GameObject gobj){
+		if ( Utility.GoHasComponent<IsGenerator>( gobj)) return true;
+		//plant condition
+		return false;
+	}
+
+
+
+	public GameObject FindTarget(){
+		GameObject target = null;
+
+		target = Utility.GetClosestWith(rigidbody.position, searchRange, IsValidTarget);
+
+		if( target == null) target = GameManager.MainGenerator;
+
+		return target;
+	}
+
+
+
 	// Use this for initialization
 	void Start() {
-		if (target == null)
-			target = GameManager.MainGenerator.rigidbody;
-		dir = Utility.FindDirNoY(transform.position, target.position);
+		if (currTarget == null)
+			currTarget = GameManager.MainGenerator;
+		dir = Utility.FindDirNoY(transform.position, currTarget.transform.position);
 		moveScript = GetComponent<CanMove>();
 	}
 	
@@ -22,14 +47,13 @@ public class IsFog : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-
-
+		currTarget = FindTarget();
 	}
 
 
 
 	void FixedUpdate() {
-		dir = Utility.FindDirNoY(transform.position, target.position);
+		dir = Utility.FindDirNoY(transform.position, currTarget.transform.position);
 		Debug.DrawRay(transform.position, dir*2);
 		moveScript.Move(dir);
 	}
