3b1471c3a05e8881ea134e0824dc32d067f136b6
jnobleza <jnobleza@ucsc.edu>
2014-03-16 16:20:06 -0700

    Light Shard harvesting visuals + conditions.

--- Orbion/Assets/Scripts/HarvestLight.cs
+++ Orbion/Assets/Scripts/HarvestLight.cs
@@ -5,40 +5,43 @@ public class HarvestLight : MonoBehaviour {
 
 	//the amount we harvest and unload
 	public int harvestAmt = 5;
-
+	
 	//used to check if we've harvested
 	private bool hasLumen = false;
-
+	
 	//range required to harvest from a lightwell
 	public float harvestRange = 2f;
-
+	
 	//range required to unload into a generator
 	public float unloadRange = 2f;
-
+	
 	public AvatarController avatarScript;
 	public bool inMethod = true;
-
-
+	public bool rising = true;
+	
+	
 	static bool IsLightWell(GameObject gobj){
 		if (gobj.tag == "lightwell") return true;
 		return false;
 	}
-
-
+	
+	
 	//switches to harvested state if we're in range of a lightwell
 	void TryHarvest(float range){
 		GameObject geyser = Utility.GetClosestWith(transform.position, range, IsLightWell);
 		if( geyser == null) return;
 		if(inMethod){
 			inMethod = false;
-			Invoke ("ReturnHasLumen", 2);
+			ReturnHasLumen ();
+			//Invoke ("ReturnHasLumen", 2);
 		}
 	}
-
+	
 	public bool HoldingLumen(){
 		return hasLumen;
 	}
 	
+	// Checks again if the player is still near, then spawn a lumen shard.
 	void ReturnHasLumen(){
 		inMethod = true;
 		GameObject geyser = Utility.GetClosestWith(transform.position, harvestRange, IsLightWell);
@@ -46,50 +49,65 @@ public class HarvestLight : MonoBehaviour {
 			Debug.Log ("hasLumen = " + hasLumen);
 			return;
 		}
-		hasLumen = true;
-		geyser.GetComponent<GiveLumen>().spawnLumen();
+		
+		if(rising){
+			geyser.GetComponent<GiveLumen>().spawnLumen();
+		}
+		rising = false;
+		//avatarScript.rasAnim ();
 		//Debug.Log ("Eridan was here");
+		//hasLumen = true;
+		
+	}
+	
+	public void setRisingTrue(){
+		rising = true;
+	}
+	
+	public void setHoldingTrue(){
+		hasLumen = true;
 		Debug.Log ("hasLumen = " + hasLumen);
 	}
 	
-
-
+	
+	
 	//unloads lumen into the closest generator in range, if any
 	void TryUnload(float range){
 		GameObject generator = Utility.GetClosestWith(transform.position, range, Utility.GoHasComponent<IsGenerator>);
 		if( generator == null) return;
 		generator.GetComponent<IsGenerator>().CurrLumen += harvestAmt;
 		hasLumen = false;
+		avatarScript.rasAnim ();
 		avatarScript.SendLightShard ();
 	}
 	
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		//hasLumen = false;
 		Debug.Log(hasLumen);
 	}
-
-
-
+	
+	
+	
 	// Update is called once per frame
 	void Update () {
-
+		
 		if(Input.GetMouseButtonDown(1)){
 			if (hasLumen) 
 				TryUnload(unloadRange);
 			else 
-				TryHarvest(harvestRange); 
-
+				TryHarvest(harvestRange);
+			
 		}
 		
 	}
-
-
+	
+	
 	void OnTriggerStay(Collider other){
 	}
- 
-
-
+	
+	
+	
 }
