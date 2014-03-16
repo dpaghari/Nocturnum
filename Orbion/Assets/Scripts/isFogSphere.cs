d1ecd015d30684dfa33104109d0ce63fdf81a033
Daniel Pagharion <dpaghari@ucsc.edu>
2014-03-16 16:20:26 -0700

    <IsFogSphere> created and pops after a timer

---
+++ Orbion/Assets/Scripts/isFogSphere.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isFogSphere : MonoBehaviour {
+
+	private float timer;
+	private float timerCD;
+	private Rigidbody clone;
+
+	public Rigidbody fog;
+	public IsFogEater fogScript;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0.0f;
+		timerCD = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer > timerCD){
+			timer = 0.0f;
+			popSphere();
+
+		}
+	
+	}
+	public void popSphere(){
+
+		int count = fogScript.fogCount;
+
+		for(int i = 0; i <= count; i++){
+
+			clone = Instantiate(fog, transform.position, Quaternion.identity) as Rigidbody;
+			Destroy(gameObject);
+		}
+	}
+}
