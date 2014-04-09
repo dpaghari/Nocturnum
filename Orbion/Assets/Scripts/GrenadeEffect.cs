e77c9ca3d0027a5b59ef7954dbdaba8ac354cd68
DannyPaguiao <dpaghari@ucsc.edu>
2014-04-08 21:01:25 -0700

    Modified LightGrenade's behavior so it pulls enemies to the center -It's kind of instantaneous right now gonna work on making it smoother prolly through <CanMove>

---
+++ Orbion/Assets/Scripts/GrenadeEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrenadeEffect : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerStay(Collider other){
+
+		if(other.tag == "Enemy" || other.tag == "EnemyRanged")
+			other.transform.position = transform.position;
+
+
+
+
+
+	}
+}
