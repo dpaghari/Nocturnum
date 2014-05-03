316769c4a7d0e5501dba316d7c258685a9e7aa99
William Situ <wsitu@ucsc.edu>
2014-05-02 18:11:08 -0700

    Scripts for hiding tall objects

---
+++ Orbion/Assets/Scripts/IsVisibleUnderObjects.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsVisibleUnderObjects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction =  transform.position - Camera.main.transform.position;
+		RaycastHit[] hits = Physics.RaycastAll(Camera.main.transform.position, direction);
+		Debug.DrawRay(Camera.main.transform.position, direction, Color.green);
+
+
+		for( int i=0; i<hits.Length; i++){
+			IsInvisibleOntopObjects visibilityScript = hits[i].collider.gameObject.GetComponent<IsInvisibleOntopObjects>();
+			if( visibilityScript != null) visibilityScript.Hide();
+		}
+	}
+}
