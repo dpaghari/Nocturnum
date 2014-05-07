814eafaf18826ce3ce2b1b634b391f0514238977
alduin <jnobleza@ucsc.edu>
2014-05-07 05:13:45 -0700

    Added reloading text back in. Started working Build Menu (not active yet).

---
+++ Orbion/Assets/Scripts/UI/ShowReloading.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowReloading : MonoBehaviour {
+	public dfLabel _label;
+	public GameObject ammoRef;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameManager.AvatarContr.shootScript.reloading){
+			_label.Text = "Reloading...";
+		} else {
+			_label.Text = "Ammo: " + ammoRef.GetComponent<CanShootReload>().currentAmmo + "/" + ammoRef.GetComponent<CanShootReload>().clipSize;
+		}
+	}
+}
