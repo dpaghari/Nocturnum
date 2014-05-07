58c065d6ee298ef9f32938c28514d96a0eee570f
alduin <jnobleza@ucsc.edu>
2014-05-07 00:44:22 -0700

    Beginning to move UI over to DF (doing this in parts because Git and scene hate each other). Will delete current UI later.

---
+++ Orbion/Assets/Scripts/UI/ShowEnergy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowEnergy : MonoBehaviour {
+	public dfLabel _label;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string energyString = string.Format("{0}/{1}", ResManager.UsedEnergy, ResManager.MaxEnergy);
+		_label.Text = "Energy: " + energyString;
+	}
+}
---
+++ Orbion/Assets/Scripts/UI/ShowLumenCollected.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowLumenCollected : MonoBehaviour {
+	public dfLabel _label;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string lumenString = string.Format("{0}", ResManager.Lumen);
+		_label.Text = "Lumen: " + lumenString;
+	}
+}
