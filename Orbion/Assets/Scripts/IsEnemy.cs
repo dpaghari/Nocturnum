0f789b0dac4875cda4b57e7b9d98df8cd4eb2c3e
Mjshaar <mj.shaar@ucsc.edu>
2014-05-07 16:06:25 -0700

    <CanSpawnUpdate>-spawns enemies in random locations <IsEnemy>-adds to metric manager count of enemies <Killable>-doesn't subtract total enemies when base_enemy_prefab(scene enemies) dies

--- Orbion/Assets/Scripts/IsEnemy.cs
+++ Orbion/Assets/Scripts/IsEnemy.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 
 public class IsEnemy : MonoBehaviour {
-
+	
+	void Start(){
+		
+		if(gameObject.name != "base_enemy_prefab"){
+			MetricManager.AddEnemies(1);
+		}
+	}
 	//Dummy script to be attached to all enemies so that other scripts can check if a gameobject is an enemy or not.
 }
