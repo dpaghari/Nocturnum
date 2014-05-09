40a4bc271fbfb7c00eece91de7c3db068c62a0dc
Mjshaar <mj.shaar@ucsc.edu>
2014-05-09 13:37:53 -0700

    Navmesh implemented - test how enemies move around environment

--- Orbion/Assets/Scripts/AiBehaviors/AB_Aggressive.cs
+++ Orbion/Assets/Scripts/AiBehaviors/AB_Aggressive.cs
@@ -37,8 +37,8 @@ public class AB_Aggressive : AiBehavior {
 				//transform.LookAt(CurrTarget.transform);
 				Vector3 lookPosition = new Vector3(CurrTarget.position.x, transform.position.y, CurrTarget.position.z);
 				transform.rotation = Quaternion.LookRotation(transform.position - lookPosition);
-				moveScript.Move(CurrTarget.position - rigidbody.position);
-				//meshScript.SetDestination(CurrTarget.position);
+				//moveScript.Move(CurrTarget.position - rigidbody.position);
+				meshScript.SetDestination(CurrTarget.position);
 
 				if(this.tag == "Enemy")
 					animation.CrossFade("WolfRunCycle");
