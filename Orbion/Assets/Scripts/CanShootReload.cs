44a8b02e1db98dfe43bc371604a018e4ea1c0300
William Situ <wsitu@ucsc.edu>
2014-05-14 16:10:31 -0700

    Added multishot bat

--- Orbion/Assets/Scripts/CanShootReload.cs
+++ Orbion/Assets/Scripts/CanShootReload.cs
@@ -32,37 +32,9 @@ public class CanShootReload : CanShoot {
 
 		if( reloading)
 			reloadTime += Time.deltaTime;
-		/*
-		if(currentAmmo <= 0)  														// if object is out of ammo
-		   reloading = true;
 
-		if(Input.GetKeyDown("r") && currentAmmo < clipSize && currentAmmo > 0)		// only called if attached to player
-		   reloading = true;
-
-		
-		if(reloading == true){
-
-			reload ();
-
-		}
-		*/
 	}
 
-
-	/*	callShoot should be called by something like AI Controller where all it does is pass in the position of the player as the target position
-	 *  which should be calculated in that script.
-	 * 
-	 *  callShoot is also called by the AvatarController script when the player presses the left mouse button passing in the mouseposition
-	 *   as the target position.
-
-	*/
-	/*
-	public void callShoot(Vector3 targ){											// call Shoot() from CanShoot component with target vec3
-		if(currentAmmo > 0 && !reloading){
-			GetComponent<CanShoot>().Shoot(targ);
-		}
-	}
-	*/
 
 
 	//if you try to fire with no ammo and you're not realoding, it will reload for you
@@ -83,20 +55,22 @@ public class CanShootReload : CanShoot {
 
 	}
 
+
+	public override void Shoot( Vector3 target){
+		if (currentAmmo <= 0)
+			Reload ();
+		else {
+			int adjustedBulletCount = Mathf.Min(numOfBulletShot, currentAmmo);
+			Scattershot( target, adjustedBulletCount);
+		}
+	}
+
+
 	public void Reload () {
 		if (reloading || currentAmmo == clipSize) return;
 
 		reloading = true;
 		reloadTime = 0;
-
-		/*
-		reloadTime++; 
-		if(reloadTime >= reloadCooldown){
-			currentAmmo = clipSize;
-			reloading = false;
-			reloadTime = 0.0F;
-		}
-		*/
 
 	}
 
