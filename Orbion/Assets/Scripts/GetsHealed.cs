52d2ad5dbee395d70fd5f2d6aa909e28611e2f59
Daniel Pagharion <dpaghari@ucsc.edu>
2014-05-14 11:36:00 -0700

    Nerfed MedBay

--- Orbion/Assets/Scripts/GetsHealed.cs
+++ Orbion/Assets/Scripts/GetsHealed.cs
@@ -3,19 +3,31 @@ using System.Collections;
 
 public class GetsHealed : MonoBehaviour {
 
+
+
+	public DumbTimer timerScript;
 	// Use this for initialization
 	void Start () {
-	
+		timerScript = DumbTimer.New(0.5f, 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		timerScript.Update();
+
 	}
 
 	void OnTriggerStay(Collider other){
-		if(other.tag == "MedBay"){
-			this.gameObject.GetComponent<Killable>().Heal(1);
+		if(timerScript.Finished() == true){
+			if(other.tag == "MedBay"){
+				this.gameObject.GetComponent<Killable>().Heal(1);
+				timerScript.Reset();
+			}	
 		}
+
+
+
+
+
 	}
 }
