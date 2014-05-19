8aec23a8d252c6c28918358938de700a5338d7f9
alduin <jnobleza@ucsc.edu>
2014-05-19 16:01:04 -0700

    Toggling script (not in yet).

---
+++ Orbion/Assets/Scripts/UI/ToggleResearchHUD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleResearchHUD : MonoBehaviour {
+	public dfTabContainer _tabContainer;
+	bool onHUD;
+
+	// Use this for initialization
+	void Start () {
+		onHUD = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ToggleHUD(){
+		_tabContainer.IsVisible = !onHUD;
+
+	}
+}
