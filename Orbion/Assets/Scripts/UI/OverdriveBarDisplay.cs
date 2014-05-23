dd6d6344b7d6d120848eee8a5d3bf1f0c6e1f06c
alduin <jnobleza@ucsc.edu>
2014-05-23 03:47:17 -0700

    Overdrive bar with temp. sprite.

---
+++ Orbion/Assets/Scripts/UI/OverdriveBarDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverdriveBarDisplay : MonoBehaviour {
+
+	public GameObject playerRef;
+	public dfProgressBar _overdriveFill;
+	public float fillBar;
+	// Use this for initialization
+	void Start () {
+		playerRef = GameObject.Find ("player_prefab");
+		fillBar = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fillBar = playerRef.GetComponent<hasOverdrive>().overdriveCount/playerRef.GetComponent<hasOverdrive>().overdriveLimit;
+		_overdriveFill.Value = fillBar;
+	}
+}
