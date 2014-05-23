c581aa9909d15fd9dee835bafb1fa6b2029242ad
DannyPaguiao <dpaghari@ucsc.edu>
2014-05-23 04:06:56 -0700

    Added luna's spaceship in scene2 Added light geysers in scene2 Added Orbion bobbing up and down

--- Orbion/Assets/Orbit.cs
+++ Orbion/Assets/Orbit.cs
@@ -7,8 +7,15 @@ public class Orbit : MonoBehaviour {
 	private Vector3 direction;
 	private Vector3 position;
 
+	float currHeight;
+	float amplitude;
+	float speed;
+
 	// Use this for initialization
 	void Start () {
+		amplitude = 1.0f;
+		speed = 1.0f;
+		currHeight = transform.position.y;
 	}
 	
 	// Update is called once per frame
@@ -16,19 +23,29 @@ public class Orbit : MonoBehaviour {
 		//transform.RotateAround (target.position, Vector3.up, degrees * Time.deltaTime * 10);
 	
 
+
 	}
 
 	void FixedUpdate(){
-		position = target.transform.position;
-		position.y += 2;
-		position.z -= 2;
-		position.x -= 2;
+		//position = target.transform.position;
+		//position.y += 2;
+		//position.z -= 2;
+		//position.x -= 2;
 
-		transform.position = position;
+		Vector3 pos = target.transform.position;
+		pos.y = currHeight+amplitude*Mathf.Sin(speed*Time.time);
+		pos.z -= 2;
+		pos.x -= 2;
+		
+		transform.position = pos;
+
+		//transform.position = position;
 
 		//transform.position = target.transform.position;
 		direction = Utility.GetMouseWorldPos(transform.position.y);
 		transform.forward = direction - transform.position;
 		//transform.rotation = Quaternion.identity;
 	}
+
 }
+
