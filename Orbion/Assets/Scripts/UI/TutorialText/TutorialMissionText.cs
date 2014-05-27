d8b93644b547424736dbd69f06f632b53e4bb74e
DannyPaguiao <dpaghari@ucsc.edu>
2014-05-27 04:47:24 -0700

    tutorial mission started

--- Orbion/Assets/Scripts/UI/TutorialText/TutorialMissionText.cs
+++ Orbion/Assets/Scripts/UI/TutorialText/TutorialMissionText.cs
@@ -6,12 +6,19 @@ public class TutorialMissionText : MonoBehaviour {
 
 	public hasOverdrive overdriveScript;
 	public DumbTimer timerScript;
+	//public DumbTimer cleartextScript;
+	public Transform tutWolfpos;
+	public GameObject demoWolf;
+	private GameObject clone;
 	bool gotLumen;
 	bool builtGenerator;
+	bool madeWolf;
 	
 	// Use this for initialization
 	void Start () {
+		madeWolf = false;
 		timerScript = DumbTimer.New(5.0f, 1.0f);
+		//cleartextScript = DumbTimer.New(5.0f, 1.0f);
 		overdriveScript = GameManager.AvatarContr.GetComponent<hasOverdrive>();
 		tutorialLine.IsVisible = true;
 		gotLumen = false;
@@ -21,6 +28,7 @@ public class TutorialMissionText : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+	//	cleartextScript.Update();
 
 		if(ResManager.Lumen <= 0 && gotLumen == false){
 			tutorialLine.Text = "Collect Lumen to build structures";
@@ -29,7 +37,7 @@ public class TutorialMissionText : MonoBehaviour {
 			tutorialLine.Text = "Press B to Open the Build Menu";
 			gotLumen = true;
 		}
-		if(gotLumen == true){
+		if(gotLumen == true && TechManager.hasGenerator == false){
 			if(builtGenerator)
 			timerScript.Update();
 			if(Input.GetKeyDown(KeyCode.B)){
@@ -42,6 +50,21 @@ public class TutorialMissionText : MonoBehaviour {
 				timerScript.Reset();
 			}
 		}
+		if(TechManager.hasGenerator && madeWolf == false){
+
+			Vector3 pos = tutWolfpos.position;
+			clone = Instantiate(demoWolf, pos, Quaternion.identity) as GameObject;
+			clone = Instantiate(demoWolf, pos, Quaternion.identity) as GameObject;
+			tutorialLine.Text = "Defeat Enemies to gain Lumen";
+			madeWolf = true;
+
+		}
+		/*
+		if(cleartextScript.Finished() == true){
+			tutorialLine.Text = "";
+			cleartextScript.Reset();
+		}
+		*/
 		if(overdriveScript.overdriveOn){
 			if(!overdriveScript.overdriveActive)
 				tutorialLine.Text = "Press SPACE to activate Overdrive!";
