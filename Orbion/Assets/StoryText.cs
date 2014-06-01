103b1a68a1eb3b17dd94e6af5dffd25565833883
DannyPaguiao <dpaghari@ucsc.edu>
2014-06-01 05:13:50 -0700

    -Updated Luminosaur Model -Nerfed player bullet(15 -> 10) -Textured Luminosaur Model -Started Story Text Screen

---
+++ Orbion/Assets/StoryText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryText : MonoBehaviour {
+
+
+	public dfLabel storyLine;
+	public DumbTimer timerScript;
+	public DumbTimer nextlevelScript;
+
+	
+	// Use this for initialization
+	void Start () {
+	
+		timerScript = DumbTimer.New(6.0f, 1.0f);
+		nextlevelScript = DumbTimer.New(10.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timerScript.Update();
+		if(timerScript.Finished() == true){
+			storyLine.Text = " Earth has become too overpopulated that world\n leaders have decided to look at alternative living in space.\n Because of how lucrative real estate has become\n due to the high demand of land and the limited availability,\n corporations from around the world race to find\n habitable planets for mankind,\n with sole ownership of a planet\n going to whichever corporation finds and terraforms it first.";		
+
+		}
+		else
+			storyLine.Text = "";
+			timerScript.Reset();
+			
+		}
+
+		
+
+}
