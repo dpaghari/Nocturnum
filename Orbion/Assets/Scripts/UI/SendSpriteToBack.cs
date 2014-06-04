f1c6db3d6330de3551d4128bade9525f947f1293
alduin <jnobleza@ucsc.edu>
2014-06-04 15:22:50 -0700

    Building Icons, and level 1 UI hopefully fixed.

---
+++ Orbion/Assets/Scripts/UI/SendSpriteToBack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendSpriteToBack : MonoBehaviour {
+	public dfSprite sprite;
+	// Use this for initialization
+	void Start () {
+		sprite.BringToFront ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
