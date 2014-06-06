3922907605c39bfd100a49ca8aed9acee60ba745
DannyPaguiao <dpaghari@ucsc.edu>
2014-06-06 02:17:54 -0700

    Applied Build Menu and Upgrade Menu Changes to Level 2,3 and tutorial Made Prefabs for UI elements to make for easier importing to each level

--- Orbion/Assets/Scripts/EquipmentBehaviors/EB_LightFist.cs
+++ Orbion/Assets/Scripts/EquipmentBehaviors/EB_LightFist.cs
@@ -15,10 +15,11 @@ public class EB_LightFist : EquipmentBehavior {
 
 
 	public override void Action ( Vector3 cursor){
-		GameManager.KeysEnabled = false;
+		GetComponent<CanMove>().MoveScale -= 1;
+		//GameManager.KeysEnabled = false;
 		//Vector3 temp = transform.position;
 		//temp.y += 4;
-		audio.PlayOneShot(fistSound, 0.5f);
+		audio.PlayOneShot(fistSound, 0.2f);
 		animation.Play("Groundpunch"); 
 		StartCoroutine(WaitAndCallback(animation["Groundpunch"].length));
 		//animation.CrossFade("Groundpunch");	
@@ -48,11 +49,12 @@ public class EB_LightFist : EquipmentBehavior {
 	IEnumerator WaitAndCallback(float waitTime){
 		yield return new WaitForSeconds(waitTime - 0.6f); 
 		Vector3 temp = transform.position;
-		temp.y += 1;
+		temp.y += 0.5f;
 		clone = Instantiate (punchObj, transform.position, Quaternion.identity) as Rigidbody;
 		Instantiate(flare, temp, Quaternion.identity);
 		Instantiate(vfx, temp, Quaternion.identity);
-		GameManager.KeysEnabled = true;
+		GetComponent<CanMove>().MoveScale += 1;
+		//GameManager.KeysEnabled = true;
 		//Debug.Log("Thishappened");
 	}
 	
