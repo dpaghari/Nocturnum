81e56116a03c6d95aecc454d528dee4dc4de722a
DannyPaguiao <dpaghari@ucsc.edu>
2014-06-10 04:43:48 -0700

    Main Menu Screen Almost Done Just need to do options and level select functionality

---
+++ Orbion/Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	private bool beatenGame;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void gostartGame(){
+
+		AutoFade.LoadLevel("difficultylevelselect", 2.0f, 2.0f, Color.black);
+	}
+	public void golevelSelect(){
+
+		//GameManager.GameDifficulty = 2;
+		//AutoFade.LoadLevel("", 2.0f, 2.0f, Color.black);
+	}
+	public void goOptions(){
+		//GameManager.GameDifficulty = 3;
+		AutoFade.LoadLevel("Options", 2.0f, 2.0f, Color.black);
+	}
+}
