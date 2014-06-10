e3aa3f3ac126fafa8b8da66a841cffe5a22c5476
William Situ <wsitu@ucsc.edu>
2014-06-09 19:29:20 -0700

    Added spherical object for blocking player building

--- Orbion/Assets/Scripts/IsBuildingObstacle.cs
+++ Orbion/Assets/Scripts/IsBuildingObstacle.cs
@@ -1,4 +1,5 @@
 ﻿//Purpose: Attach to an object to prevent building near/on this object
+//Note: y position of this object is ignored, so assume y = 0
 
 using UnityEngine;
 using System.Collections;
@@ -7,4 +8,12 @@ public class IsBuildingObstacle : MonoBehaviour {
 
 	//Radius of the circular region that buildings cannot be built in
 	public float contactRadius = 2.0f;
+
+	//Applies x scale of object to contact radius
+	//If this is checked, contact radius must be fitted while it's scales is 1
+	public bool applyScale= false;
+
+	void Start(){
+		contactRadius = contactRadius * transform.localScale.x;
+	}
 }
