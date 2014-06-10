fd5772fcdef71227dac5a500daad079a4db3048c
Mjshaar <mj.shaar@ucsc.edu>
2014-06-10 01:44:30 -0700

    MetricManager Tracks more stuff now

--- Orbion/Assets/Scripts/Managers/MetricManager.cs
+++ Orbion/Assets/Scripts/Managers/MetricManager.cs
@@ -9,15 +9,31 @@ public class MetricManager : Singleton<MetricManager> {
 	private int _enemiesKilled = 0;
 	private int _totalEnemies = 0;
 	private int _totalWaves = 0;
+	private int _enemiesHit = 0;
+	private int _buildingsHit = 0;
+	private int _buildingsHealed = 0;
+
+	public static void AddBuildingsHealed(int amt){ Instance._buildingsHealed += amt;}
+	public static int getBuildingsHealed{ get { return Instance._buildingsHealed;}}
+
+	public static void AddBuildingsHit(int amt){ Instance._buildingsHit += amt;}
+	public static int getBuildingsHit{ get { return Instance._buildingsHit;}}
+
+	public static void AddEnemiesHit(int amt){ Instance._enemiesHit += amt;}
+	public static int getEnemiesHit{ get { return Instance._enemiesHit;}}
 
 	public static void AddShots(int amt){ Instance._shotsFired += amt;}
 	public static int getShotsFired{ get { return Instance._shotsFired;}}
 
-	public static void AddWaves(int amt){ Instance._shotsFired += amt;}
-	public static int getTotalWaves{ get { return Instance._shotsFired;}}
+	public static void AddWaves(int amt){ Instance._totalWaves += amt;}
+	public static int getTotalWaves{ get { return Instance._totalWaves;}}
 
 	public static void AddEnemiesKilled(int amt){ Instance._enemiesKilled += amt;}
 	public static int getEnemiesKilled{ get { return Instance._enemiesKilled;}}
+
+	
+
+
 
 	public static void AddEnemies(int amt){ 
 		Instance._totalEnemies += amt;
@@ -33,6 +49,7 @@ public class MetricManager : Singleton<MetricManager> {
 		Instance._enemiesKilled = 0;
 		Instance._totalEnemies = 0;
 		Instance._totalWaves = 0;
+		Instance._enemiesHit = 0;
 		
 	}
 	/*
