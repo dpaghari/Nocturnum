7bca414d012b40c139e0cc7c5c7462c835fb1a42
William Situ <wsitu@ucsc.edu>
2014-06-10 21:45:51 -0700

    Adjusted scaling for scattershot and lightfist

--- Orbion/Assets/Scripts/EquipmentBehaviors/EB_LightFist.cs
+++ Orbion/Assets/Scripts/EquipmentBehaviors/EB_LightFist.cs
@@ -6,7 +6,7 @@ using System.Collections;
 
 public class EB_LightFist : EquipmentBehavior {
 
-
+	public int fistDamage = 20;
 	public Rigidbody punchObj;
 	private Rigidbody clone;
 	public GameObject flare;
@@ -22,13 +22,25 @@ public class EB_LightFist : EquipmentBehavior {
 	}
 
 
+	public void UpdateLevelAdjustments(GameObject punchObj, GameObject flareObj, GameObject vfxObj){
+		int fistLevel = TechManager.GetUpgradeLv(Tech.lightFist);
+		float sizeScale = 1 + 0.1f * fistLevel;
+		float pushScale = 1 + 0.5f * fistLevel;
+		isFistShockwave fistScript = punchObj.GetComponent<isFistShockwave>();
+		fistScript.fistDamage = fistDamage + 5 * fistLevel;
+		fistScript.pushForce *= pushScale;
+		punchObj.transform.localScale *= sizeScale;
+		flareObj.particleSystem.startSize *= sizeScale;
+	}
+
 	public void Shockwave(){
 
 		Vector3 temp = transform.position;
 		temp.y += 0.5f;
 		clone = Instantiate (punchObj, transform.position, Quaternion.identity) as Rigidbody;
-		Instantiate(flare, temp, Quaternion.identity);
-		Instantiate(vfx, temp, Quaternion.identity);
+		GameObject flareClone = Instantiate(flare, temp, Quaternion.identity) as GameObject;
+		GameObject vfxClone = Instantiate(vfx, temp, Quaternion.identity) as GameObject;
+		UpdateLevelAdjustments( clone.gameObject, flareClone, vfxClone);
 		isPlayingAudio = false;
 	}
 
