2359caba6f462b39eb9861b276f340ea0c1d801a
Daniel Pagharion <dpaghari@ucsc.edu>
2014-06-10 18:06:51 -0700

    Level Select Implemented Saves to PlayerPrefs Medbay and Light Grenade Buttons fixed Still need to reattach Light Grenade to Tab Container elements Created SaveManager

---
+++ Orbion/Assets/isLevelOption.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isLevelOption : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void goTutorial(){
+		AutoFade.LoadLevel("tutorial", 2.0f, 2.0f, Color.black);
+	}
+	public void golevel1(){
+		AutoFade.LoadLevel("level1", 2.0f, 2.0f, Color.black);
+	}
+	public void golevel2(){
+		AutoFade.LoadLevel("level2", 2.0f, 2.0f, Color.black);
+	}
+	public void golevel3(){
+		AutoFade.LoadLevel("level3", 2.0f, 2.0f, Color.black);	
+	}
+	public void golevel4(){
+		AutoFade.LoadLevel("level4", 2.0f, 2.0f, Color.black);
+	}
+	public void golevel5(){
+		AutoFade.LoadLevel("BatBossBattle", 2.0f, 2.0f, Color.black);
+	}
+}
---
+++ Orbion/Assets/isLevelSelect.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isLevelSelect : MonoBehaviour {
+
+	public dfButton tutButton, lvl1Butt, lvl2Butt, lvl3Butt, lvl4Butt, lvl5Butt;
+
+	private int numLevelstoLoad;
+	// Use this for initialization
+	void Start () {
+		numLevelstoLoad = SaveManager.LoadSavedLevels();
+
+		tutButton.IsVisible = false;
+		lvl1Butt.IsVisible = false;
+		lvl2Butt.IsVisible = false;
+		lvl3Butt.IsVisible = false;
+		lvl4Butt.IsVisible = false;
+		lvl5Butt.IsVisible = false;
+
+
+		tutButton.IsEnabled = false;
+		lvl1Butt.IsEnabled = false;
+		lvl2Butt.IsEnabled = false;
+		lvl3Butt.IsEnabled = false;
+		lvl4Butt.IsEnabled = false;
+		lvl5Butt.IsEnabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log(numLevelstoLoad);
+		if(numLevelstoLoad >= 1){
+			tutButton.IsVisible = true;
+			tutButton.IsEnabled = true;
+		}
+		if(numLevelstoLoad >= 2){
+			lvl1Butt.IsVisible = true;
+			lvl1Butt.IsEnabled = true;
+		}
+		if(numLevelstoLoad >= 3){
+			lvl2Butt.IsVisible = true;
+			lvl2Butt.IsEnabled = true;
+		}
+		if(numLevelstoLoad >= 4){
+			lvl3Butt.IsVisible = true;
+			lvl3Butt.IsEnabled = true;
+		}
+		if(numLevelstoLoad >= 5){
+			lvl4Butt.IsVisible = true;
+			lvl4Butt.IsEnabled = true;
+		}
+		if(numLevelstoLoad >= 6){
+			lvl5Butt.IsVisible = true;
+			lvl5Butt.IsEnabled = true;
+		}
+		//Debug.Log(numLevelstoLoad)
+	}
+
+
+
+
+
+
+}
